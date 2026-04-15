7bcc22a3dedb871e859fbd0402b6c56aa647c70e
geoffreyakb <geoffreyakbar@gmail.com>
2026-04-16 01:02:47 +0200

    added very basic postprocessing

--- postproc.py
+++ postproc.py
@@ -2,5 +2,87 @@ from src import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-# vtk_path = "vtk/"
-# rho, v_r, v_theta, v_phi = READ_VTK(vtk_path, 0)
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Fourier"
+})
+w = 1.25
+l = 10
+l_log = 6
+
+# jobname = os.path.basename(os.getcwd())
+# conf = inifix.load(f'./plots/ini_file_{jobname}.ini')
+conf = inifix.load("idefix.ini")
+n_average = int(conf["TimeIntegrator"]["tstop"] / conf["Output"]["analysis"]) + 1
+n_vtk = int(conf["TimeIntegrator"]["tstop"] / conf["Output"]["vtk"]) + 1
+n_r = conf["Grid"]["X1-grid"][2]
+r_min = conf["Grid"]["X1-grid"][1]
+r_max = conf["Grid"]["X1-grid"][-1]
+
+t = READ_BOX_AVERAGE()      # REMEMBER TO CHANGE THE FUNCTION
+r, Sigma, Tilt, Precession, L = READ_RADIAL_AVERAGE(n_average, n_r)
+
+TiltMean = np.zeros(t.size)
+PrecessionMean = np.zeros(t.size)
+for i in range(t.size):
+    TiltMean[i] = np.mean(Tilt[i,:])
+    PrecessionMean[i] = np.mean(Precession[i,:])
+
+fig, axs = plt.subplots(1, 2, figsize=(13, 6))
+
+ax = axs[0]
+ax.plot(t, TiltMean, color='tab:blue')
+ax.set_xlim((0, 100))
+min_graph = 12
+max_graph = 15
+ax.set_ylim((min_graph, max_graph))
+ax.tick_params(axis='y', which='both', direction='in', right=True, width=w, length=l)
+ax.tick_params(axis='x', which='both', direction='in', top=True, width=w, length=l)
+for spine in ax.spines.values():
+        spine.set_linewidth(w)
+ax.set_xlabel(r"$t$ [Code Units]")
+ax.set_ylabel(r"Inclination [°]")
+ax.grid()
+
+ax = axs[1]
+ax.plot(t, PrecessionMean, color='tab:green')
+ax.set_xlim((0, 100))
+min_graph = 0
+max_graph = 10
+ax.set_ylim((min_graph, max_graph))
+ax.tick_params(axis='y', which='both', direction='in', right=True, width=w, length=l)
+ax.tick_params(axis='x', which='both', direction='in', top=True, width=w, length=l)
+for spine in ax.spines.values():
+        spine.set_linewidth(w)
+ax.set_xlabel(r"$t$ [Code Units]")
+ax.set_ylabel(r"Precession [°]")
+ax.grid()
+
+fig.tight_layout()
+plt.savefig("./plots/angles.pdf")
+plt.close()
+
+
+fig, axs = plt.subplots(1, 1, figsize=(6, 4))
+ax = axs
+ax.plot(r, Sigma[0,:], color='tab:blue', label=r"$t = $" + f" {t[0]:0.2e}")
+ax.plot(r, Sigma[t.size//4,:], color='tab:red', label=r"$t = $" + f" {t[t.size//4]:0.2e}")
+ax.plot(r, Sigma[t.size//2,:], color='tab:green', label=r"$t = $" + f" {t[t.size//2]:0.2e}")
+ax.plot(r, Sigma[3*t.size//4,:], color='tab:orange', label=r"$t = $" + f" {t[3*t.size//4]:0.2e}")
+ax.plot(r, Sigma[t.size-1,:], color='tab:purple', label=r"$t = $" + f" {t[t.size-1]:0.2e}")
+ax.set_xlim((r_min, r_max))
+min_graph = 12
+max_graph = 15
+# ax.set_ylim((min_graph, max_graph))
+ax.tick_params(axis='y', which='both', direction='in', right=True, width=w, length=l)
+ax.tick_params(axis='x', which='both', direction='in', top=True, width=w, length=l)
+for spine in ax.spines.values():
+        spine.set_linewidth(w)
+ax.set_xlabel(r"$r$ [Code Units]")
+ax.set_ylabel(r"Surface Density [Code Units]")
+ax.grid()
+ax.legend()
+
+fig.tight_layout()
+plt.savefig("./plots/sigma.pdf")
+plt.close()
--- src.py
+++ src.py
@@ -18,7 +18,7 @@ import numpy.fft as fft
 from scipy.stats import binned_statistic
 import csv
 
-def READ_BOX_AVERAGES():
+def READ_BOX_AVERAGE():
     fid = open("output/analysis/globalAverage.dat", "r")
     varnames = fid.readline().split()
     fid.close()
@@ -26,34 +26,69 @@ def READ_BOX_AVERAGES():
     V = {}
     i = 0
     for name in varnames:
-        V[name] = data[:,i]
+        # V[name] = data[:,i]           # TO CHANGE WHENEVER GLOBAL QUANTITIES ARE ADDED
+        V[name] = data[:]
         i += 1
 
-    return V
+    return V["t"]
 
-def READ_VTK(vtk_path, t):
+def READ_RADIAL_AVERAGE(n_average, n_r):
+    Sigma = np.zeros((n_average, n_r))
+    Tilt = np.zeros((n_average, n_r))
+    Precession = np.zeros((n_average, n_r))
+    L = np.zeros((n_average, n_r, 3))
+
+    current_number = 0
+    for i in range(n_average):
+        if i < 10:
+            current_number = f"000{i}"
+        elif i < 100:
+            current_number = f"00{i}"
+        elif i < 1000:
+            current_number = f"0{i}"
+        else:
+            current_number = i
+
+        fid=open(f"output/analysis/radialAverage_{current_number}.dat","r")
+        varnames=fid.readline().split()
+        fid.close()
+        data=np.loadtxt(f"output/analysis/radialAverage_{current_number}.dat",skiprows=1)
+        V={}
+        j=0
+        for name in varnames:
+            V[name]=data[:,j]
+            j=j+1
+
+        Sigma[i,:] = V['Sigma']
+        Tilt[i,:] = V['Tilt']
+        Precession[i,:] = V['Precession']
+        L[i,:,0] = V['Lx']
+        L[i,:,1] = V['Ly']
+        L[i,:,2] = V['Lz']
+
+    return V["r"], Sigma, Tilt, Precession, L
+
+def READ_VTK(n_vtk):
     NVAR = 4
     RHO = 0
     VX1 = 1
     VX2 = 2
     VX3 = 3
 
-    if t >= 1000:
-        current_number = str(t)
-    elif t >= 100:
-        current_number = '0' + str(t)
-    elif t >= 10:
-        current_number = '00' + str(t)
+    if n_vtk >= 1000:
+        current_number = str(n_vtk)
+    elif n_vtk >= 100:
+        current_number = '0' + str(n_vtk)
+    elif n_vtk >= 10:
+        current_number = '00' + str(n_vtk)
     else:
-        current_number = '000' + str(t)
-    current_VTK = readVTK(vtk_path + 'data.' + current_number + '.vtk', geometry='spherical')    
-    # print(vars(current_VTK))
-    # Coordinates
+        current_number = '000' + str(n_vtk)
+    current_VTK = readVTK('output/vtk/data.' + current_number + '.vtk', geometry='spherical')    
+    
     r_vtk = current_VTK.r
     theta_vtk = current_VTK.theta
     phi_vtk = current_VTK.phi
-    # x,y,z aussi
-
+    # x,y,z aussi !!!!!!!!!!!!!!!!!!!!!!!
 
     vtk = np.zeros((NVAR, phi_vtk.size, theta_vtk.size, r_vtk.size), dtype=float)
     vtk[RHO,:,:,:] = np.moveaxis(current_VTK.data['RHO'], [0, 2], [2, 0])
